5c75695b4ac5eaacfe48e554a46d4719d4a297a9
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-03-29 16:31:35 +0530

    Building backend api

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,10 +1,21 @@
 "use client"
 import React from "react";
+import { useContext } from "react";
+import { ecomContext } from "../contexts/ecomContext";
+import ProductCard from "../components/productCard";
 
-export default function Cart(cart){
+export default function Cart(){
+    const {cart}=useContext(ecomContext)
     return(
         <div>
-            <Cart/> 
+            {cart.map((product,index)=>{
+                return <ProductCard
+                    key={index}
+                    image={product.image}
+                    title={product.title}
+                    desc={product.desc}
+                />
+            })}
         </div>
     );
 }
